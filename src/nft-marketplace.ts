4532b4af0b755c32dc51290fdbfcec8a4fbc1261
FurkanSezal <fsezal46@gmail.com>
2023-07-04 22:01:24 +0300

    Initial commit

---
+++ src/nft-marketplace.ts
@@ -0,0 +1,78 @@
+import {
+  ItemBought as ItemBoughtEvent,
+  ItemCanceled as ItemCanceledEvent,
+  ItemListed as ItemListedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent
+} from "../generated/NftMarketplace/NftMarketplace"
+import {
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  OwnershipTransferred
+} from "../generated/schema"
+
+export function handleItemBought(event: ItemBoughtEvent): void {
+  let entity = new ItemBought(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.buyer = event.params.buyer
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+  entity.pieces = event.params.pieces
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemCanceled(event: ItemCanceledEvent): void {
+  let entity = new ItemCanceled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.seller = event.params.seller
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.pieces = event.params.pieces
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemListed(event: ItemListedEvent): void {
+  let entity = new ItemListed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.seller = event.params.seller
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+  entity.pieces = event.params.pieces
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
